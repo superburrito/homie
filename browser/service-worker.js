4d59209e34306f9fbbcdfbd1cf2dae9c040b14bb
superburrito <chuayaohui@gmail.com>
2017-03-13 23:25:42 +0800

    bugfix landing

--- browser/service-worker.js
+++ browser/service-worker.js
@@ -1,6 +1,6 @@
 // Cache for application shell
-const apiCacheName = "HomieAPICache-0.3.27";
-const shellCacheName = "HomieShellCache-0.3.27";
+const apiCacheName = "HomieAPICache-0.3.29";
+const shellCacheName = "HomieShellCache-0.3.29";
 
 var filesToCache = [
 	// External dependencies (npm and bower)
